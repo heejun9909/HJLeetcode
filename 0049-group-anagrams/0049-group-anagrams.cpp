a239be3a5a3ff45de8faaa42311d2ede7d64688a
heejun <47299103+heejun9909@users.noreply.github.com>
2024-02-22 21:27:27 +0800

    Update 0049-group-anagrams.cpp

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,3 +1,7 @@
+// Idea is to create local array for each loop for the anagram and push the local array to the final array after the end of loop
+// First send the 0th element of the input to the local array for comparison and check if anything in the input is anagram of the 0th element of the local array
+// If anagram is found, use .erase() function to erase the element and shrink the size of the input
+// Do the loop until input array size is zero
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -62,3 +66,57 @@ public:
         
     }
 };
+
+//-------------------------------------------------------------------------------------------------------------//
+// Better solution from LeetCode
+// Idea is to create an unordered map to store representative key for every following anagrams
+// for ex) representative key for eat, tea, ate is aet -> sorted
+// and store a vector for that particular key to store all the anagrams
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        unordered_map<string, vector<string>> mp;
+
+        // loop through the input strs to store all the representative keys by sorting the words
+        // push back all the words that belong to the key
+        for(auto x:strs)
+        {
+            string word = x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
+        }
+
+        // create a final vector to store the value (vector of strings) for corresponding keys
+        vector<vector<string>> ans;
+        for(auto y:mp)
+        {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
