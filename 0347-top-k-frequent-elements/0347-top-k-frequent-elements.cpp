bba7fde37088e61b060fe98e3595980c3d828226
heejun <47299103+heejun9909@users.noreply.github.com>
2024-02-24 18:50:09 +0800

    Update 0347-top-k-frequent-elements.cpp

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -27,3 +27,35 @@ public:
         return ans;
     }
 };
+
+//--------------------------------------------------------------------------------------------//
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int>                         ans;
+        unordered_map<int, vector<int>>     mp1;
+
+        // Using priority queue
+        // priority queue will store the biggest key as the root node and down to smallest key as default
+        priority_queue<pair<int,int>> pq;
+
+        for(auto x:nums)
+        {
+            mp1[x].push_back(x);
+        }
+
+        for(auto y:mp1)
+        {
+            pq.push(make_pair((y.second).size(), y.first));
+        }
+
+        for(int i=0;i<k;++i)
+        {
+            // pq.top().second will pull out the values of the biggest keys for k times
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
