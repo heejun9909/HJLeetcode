e29883a3f219c0e19c18daf6c4ae8ac555564642
heejun <47299103+heejun9909@users.noreply.github.com>
2024-03-28 17:53:30 +0800

    Update 0084-largest-rectangle-in-histogram.cpp

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        // Create a helper stack to store the pairs that hold (index:value) of the elements from the heights input
         stack<pair<int,int>> helper; // index:value
         int ans = 0;
 
@@ -10,10 +11,15 @@ public:
             {
                 helper.push(make_pair(i, heights[i]));
             }
+
+            // iterating from the first element, if the upcoming element is smaller than the helper.top(),
+            // it means the width of the helper.top() is restricted to that new element's index 
+            // which means the maximum area of helper.top() can be calculated
             else if(heights[i] < helper.top().second)
             {
                 int start;
-                while(!helper.empty() && heights[i] < helper.top().second)
+                while(!helper.empty() && heights[i] < helper.top().second)     // if !helper.empty() is after the (heights[i] < helper.top().second) condition, runtime error occurs.
+                                                                               // because the AND operator will check the first condition and then check for the second one.
                 {
                     int area = ((i - helper.top().first) * helper.top().second);
 
@@ -29,6 +35,8 @@ public:
             }
         }
 
+        // check for the elements that might be left over in the stack.
+        // the width is (heights.size() - index of that element)
         while(!helper.empty())
         {
             int area = ((heights.size() - helper.top().first) * helper.top().second);
