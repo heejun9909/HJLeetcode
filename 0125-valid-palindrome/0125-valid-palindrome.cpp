8eb33d4c4f0ff2e2cbf2ae24cb61a32bd6ca155e
heejun <47299103+heejun9909@users.noreply.github.com>
2024-03-28 19:34:11 +0800

    Time: 4 ms (80.26%), Space: 8.4 MB (97.41%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size()-1;
+
+        for(auto& x:s)
+        {
+            x = tolower(x);
+        }
+
+        if(s.size() == 1)
+        {
+            return true;
+        }
+
+        while(left < right)
+        {
+            while(left < right && isalpha(s[left]) == 0 && isdigit(s[left]) == 0)
+            {
+                left++;
+            }
+        
+            while(left < right && isalpha(s[right]) == 0 && isdigit(s[right]) == 0)
+            {
+                right--;
+            }
+        
+            if(isalpha(s[left]) == 0 && isalpha(s[right]) == 0 && isdigit(s[left]) == 0 && isdigit(s[right]) == 0)
+            {
+                return true;
+            }
+            else if(s[left] != s[right])
+            {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
